3a5e8b6d91945f3438ec97ecad4793eb4487dcaa
Oliver Michels <oliver.michels@gmail.com>
2023-09-09 20:46:23 +0200

    better codestyle

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 
-resource "azurerm_resource_group" "staticwebresource" {
+resource "azurerm_resource_group" "this" {
   name     = var.resource_group-name
   location = var.location
 
@@ -12,14 +12,11 @@ resource "random_string" "random_suffix" {
   upper   = false
 }
 
-locals {
-  unique_storage = "${var.storage_account_name}${random_string.random_suffix.result}"
-}
 
-resource "azurerm_storage_account" "staticwebstorage" {
-  name                     = local.unique_storage
+resource "azurerm_storage_account" "this" {
+  name                     = "${var.storage_account_name}${random_string.random_suffix.result}"
   location                 = var.location
-  resource_group_name      = var.resource_group-name
+  resource_group_name      = azurerm_resource_group.this.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
   account_kind             = "StorageV2"
@@ -30,18 +27,15 @@ resource "azurerm_storage_account" "staticwebstorage" {
 
   }
 
-  depends_on = [ azurerm_resource_group.staticwebresource ]
-
 }
 
-resource "azurerm_storage_blob" "staticwebblob" {
+resource "azurerm_storage_blob" "this" {
   name                   = "index.html"
-  storage_account_name   = local.unique_storage
+  storage_account_name   = azurerm_storage_account.this.name
   storage_container_name = "$web"
   type                   = "Block"
   content_type           = "text/html"
   source_content         = "<h1> Tolu is deploying a static web on azure storage using terraform </hi>"
 
-  depends_on = [ azurerm_storage_account.staticwebstorage ]
 
 }
